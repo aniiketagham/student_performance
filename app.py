0656834d0833adc0df107c0621b800e3cdb10aa0
AniketAgham <aniketagham1509@gmail.com>
2025-04-24 15:42:33 +0530

    data ingestion

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from src.Student_Performance.logger import logging
 from src.Student_Performance.exception import CustomException
-#from src.Student_Performance.components.data_ingestion import DataIngestion
-#from src.Student_Performance.components.data_ingestion import DataIngestionConfig
+from src.Student_Performance.components.data_ingestion import DataIngestion
+from src.Student_Performance.components.data_ingestion import DataIngestionConfig
 #from src.Student_Performance.components.data_transformation import DataTransformationConfig,DataTransformation
 #from src.Student_Performance.components.model_tranier import ModelTrainerConfig,ModelTrainer
 
@@ -11,10 +11,11 @@ import sys
 if __name__=="__main__":
     logging.info("The execution has started")
 
-    #try:
+    try:
         ##data_ingestion_config=DataIngestionConfig()
-        #data_ingestion=DataIngestion()
-        #train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+        data_ingestion=DataIngestion()
+        #train_data_path,test_data_path=
+        data_ingestion.initiate_data_ingestion()
 
         ##data_transformation_config=DataTransformationConfig()
         #data_transformation=DataTransformation()
@@ -24,8 +25,8 @@ if __name__=="__main__":
 
         #model_trainer=ModelTrainer()
         #print(model_trainer.initiate_model_trainer(train_arr,test_arr))
-    try:
-        a=1/0    
+    
+   
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
